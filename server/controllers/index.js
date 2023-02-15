016d18ef3c233839aa0a0c9a67224816ed35d167
wingchijoe <wingchijoe@gmail.com>
2023-02-14 23:06:35 -0500

    can't add edit delete before login

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,11 @@
 let express = require("express");
 let router = express.Router();
+let mongoose = require("mongoose");
+let passport = require("passport");
+
+// create the User Model instance
+let userModel = require("../models/user");
+let User = userModel.User; // alias
 
 module.exports.displayHomePage = (req, res, next) => {
   res.render("content", { title: "Home" });
@@ -19,4 +25,64 @@ module.exports.displayServicesPage = (req, res, next) => {
 
 module.exports.displayContactPage = (req, res, next) => {
   res.render("contact", { title: "Contact" });
+};
+
+module.exports.displayLoginPage = (req, res, next) => {
+  // check if the user is already logged in
+  if (!req.user) {
+    res.render("auth/login", {
+      title: "Login",
+      messages: req.flash("loginMessage"),
+      displayName: req.user ? req.user.displayName : "",
+    });
+  } else {
+    return res.redirect("/");
+  }
+};
+
+module.exports.processLoginPage = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    // server err?
+    if (err) {
+      return next(err);
+    }
+    // is there a user login error?
+    if (!user) {
+      req.flash("loginMessage", "Authentication Error");
+      return res.redirect("/login");
+    }
+    req.login(user, (err) => {
+      // server error?
+      if (err) {
+        return next(err);
+      }
+
+      const payload = {
+        id: user._id,
+        displayName: user.displayName,
+        username: user.username,
+        email: user.email,
+      };
+
+      const authToken = jwt.sign(payload, DB.Secret, {
+        expiresIn: 604800, // 1 week
+      });
+
+      /* TODO - Getting Ready to convert to API
+          res.json({success: true, msg: 'User Logged in Successfully!', user: {
+              id: user._id,
+              displayName: user.displayName,
+              username: user.username,
+              email: user.email
+          }, token: authToken});
+          */
+
+      return res.redirect("/business");
+    });
+  })(req, res, next);
+};
+
+module.exports.performLogout = (req, res, next) => {
+  req.logout();
+  res.redirect("/Login");
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,17 @@ let Business = require("../models/webuser");
 
 let indexController = require("../controllers/index");
 let businessController = require("../controllers/business");
+
+let passport = require("passport");
+// helper function for guard purposes
+function requireAuth(req, res, next) {
+  // check if the user is logged in
+  if (!req.isAuthenticated()) {
+    return res.redirect("/login");
+  }
+  next();
+}
+
 /* GET home page. */
 router.get("/", indexController.displayHomePage);
 
@@ -27,23 +38,39 @@ router.get("/contact", indexController.displayContactPage);
 router.get("/business", businessController.displayBusiness);
 
 // get route for displaying the add page - Create Operation
-router.get("/business/add", businessController.displayAddPage);
+router.get("/business/add", requireAuth, businessController.displayAddPage);
 
 // post route for processing the add page -Create Operation
-router.post("/business/add", businessController.processAddPage);
+router.post("/business/add", requireAuth, businessController.processAddPage);
 
 // get route for displaying the edit page   - Update Operation
-router.get("/business/edit/:id", businessController.displayEditPage);
+router.get(
+  "/business/edit/:id",
+  requireAuth,
+  businessController.displayEditPage
+);
 
 // post route for processing the edit page  - Update Operation
-router.post("/business/edit/:id", businessController.processEditPage);
+router.post(
+  "/business/edit/:id",
+  requireAuth,
+  businessController.processEditPage
+);
 
 // get route for displaying  the delete page
-router.get("/business/delete/:id", businessController.performDelete);
+router.get(
+  "/business/delete/:id",
+  requireAuth,
+  businessController.performDelete
+);
 
-/* GET Login Us page. */
-router.get("/Login", function (req, res, next) {
-  res.render("auth/login", { title: "Login" });
-});
+/* GET Route for displaying the Login page */
+router.get("/login", indexController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post("/login", indexController.processLoginPage);
+
+/* GET to perform UserLogout */
+router.get("/logout", indexController.performLogout);
 
 module.exports = router;
